ff306d38cd44698d1e1f0f87342472f2724815e3
Jonny <jonny.braten@jobzone.no>
2019-05-03 08:54:35 +0200

    Styling
    * Added some scss to test transpiling.
    * Added bootstrap.
    * Clicking the button now simply refreshes the joke on the current tab.

--- src/js/components/presentational/NorrisDisplayer.jsx
+++ src/js/components/presentational/NorrisDisplayer.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
+import './NorrisDisplayer.scss';
+
 const NorrisDisplayer = ({ joke, getJoke }) => (
-    <div className="form-group">
-    {joke && <h1>{joke}</h1>}
-    <button onClick={getJoke}>Get Joke</button>
+    <div className="norris-displayer">
+        {joke && <h1>{joke}</h1>}
+        <button type="button"
+            className="btn btn-outline-info"
+            onClick={() => getJoke(false)}>
+            Get Joke
+        </button>
     </div>
 );
 NorrisDisplayer.propTypes = {
